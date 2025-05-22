e1777a42682e4892c64c6738cbf6685997a655f4
Saga9103 <146200001+Saga9103@users.noreply.github.com>
2025-05-23 00:23:54 +0200

    Add files via upload

---
+++ examples/llm_example.py
@@ -0,0 +1,27 @@
+from llm_backend_async2 import AssistantEngine, logger
+
+
+def main():
+    engine = AssistantEngine()
+    engine.start()
+
+    while engine.is_running():
+        try:
+            user_input = input("\nYou: ").strip()
+            engine(user_input)
+            if user_input.lower() == "exit":
+                engine.stop()
+            elif user_input.lower() == "status":
+                print(engine.status())
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            logger.error(f"Error in AssistantEngine : {e}")
+
+    if engine.is_running():
+        logger.info("Stopping AssistantEngine")
+        engine.stop()
+
+
+if __name__ == "__main__":
+    main()
