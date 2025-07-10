c586738faab55ce4c7e73e9fddf9067a53d95721
Saga9103 <saga9103@users.noreply.github.com>
2025-07-10 18:20:14 +0200

    circular buffer instead of np.roll()

--- t2yLLM/owwDetector.py
+++ t2yLLM/owwDetector.py
@@ -1,4 +1,7 @@
+"""class responsible for getting openwakeword classifier results"""
+
 import numpy as np
+from typing import Union
 import onnxruntime as ort
 import time
 import math
@@ -8,6 +11,95 @@ from typing import Optional
 
 RATE = 16_000
 PCM_SEC = RATE
+
+
+class MDCBuffer:
+    """circular buffer accepting multi dim inputs
+    instead of np.roll() copies"""
+
+    def __init__(self, shape: Union[int, tuple], dtype: np.dtype = np.float32):
+        if isinstance(shape, int):
+            shape = (shape,)
+        self.shape = shape
+        self.dtype = dtype
+        self.buffer = np.zeros(shape, dtype=dtype)
+        self.index = 0
+        self.size = shape[0]
+        self.processed = False
+
+    def ins(self, data: np.ndarray):
+        data = np.asarray(data, dtype=self.dtype)
+        if data.ndim == 0:
+            self.buffer[self.index] = data
+            self.index = (self.index + 1) % self.size
+            if self.index == 0:
+                self.processed = True
+        if data.ndim == 1:
+            if self.buffer.ndim == 1:
+                for val in data:
+                    self.buffer[self.index] = val
+                    self.index = (self.index + 1) % self.size
+                    if self.index == 0:
+                        self.processed = True
+            else:
+                self.buffer[self.index] = data
+                self.index = (self.index + 1) % self.size
+                if self.index == 0:
+                    self.processed = True
+        else:
+            for row in data:
+                self.buffer[self.index] = row
+                self.index = (self.index + 1) % self.size
+                if self.index == 0:
+                    self.processed = True
+
+    def get(self):
+        """
+        gets buffer data in chronological order
+        """
+        if not self.processed:
+            return self.buffer[: self.index].copy()
+        else:
+            if self.buffer.ndim == 1:
+                return np.concatenate(
+                    [self.buffer[self.index :], self.buffer[: self.index]]
+                )
+            else:
+                return np.concatenate(
+                    [self.buffer[self.index :], self.buffer[: self.index]], axis=0
+                )
+
+    def __getitem__(self, key):
+        """
+        gets buffer data data in storage order
+        """
+        return self.buffer[key]
+
+    def getpart(self, k: int):
+        """
+        gets k last parts of data (chronological)
+        """
+        if k > self.size:
+            k = self.size
+        if not self.processed and k > self.index:
+            k = self.index
+
+        if k == 1:
+            return self.buffer[(self.index - 1) % self.size]
+        else:
+            seg = [(self.index - i - 1) % self.size for i in range(k)]
+            return self.buffer[seg[::-1]]
+
+    def isproc(self):
+        return self.processed
+
+    def clear(self):
+        self.buffer.fill(0)
+        self.index = 0
+        self.processed = False
+
+    def __len__(self):
+        return self.size if self.processed else self.index
 
 
 class OwwDetector:
@@ -57,8 +149,9 @@ class OwwDetector:
         self.cooldown = cooldown_ms / 1000.0
 
         self.emb_size = self.emb.get_outputs()[0].shape[-1]
-        self._pcm_buffer = np.zeros(PCM_SEC, dtype=np.float32)
-        self._emb_buffer = np.zeros((self.window, self.emb_size), dtype=np.float32)
+
+        self._pcm_buffer = MDCBuffer(PCM_SEC, dtype=np.float32)
+        self._emb_buffer = MDCBuffer((self.window, self.emb_size), dtype=np.float32)
 
         self._pcm_samples_processed = 0
         self._emb_frames_collected = 0
@@ -72,6 +165,7 @@ class OwwDetector:
 
     @staticmethod
     def _to_16k_mono(block: np.ndarray, src_rate: int) -> np.ndarray:
+        """resamples input signal converting it 16kHz mono"""
         if block.ndim == 2:
             block = block.mean(axis=1).astype(np.int16)
         if src_rate == RATE:
@@ -88,18 +182,14 @@ class OwwDetector:
     @profile
     def _pcm_to_embedding(self, pcm16: np.ndarray) -> Optional[np.ndarray]:
         pcm_float = pcm16.astype(np.float32) / 32768.0
-
-        self._pcm_buffer = np.roll(self._pcm_buffer, -len(pcm_float))
-        self._pcm_buffer[-len(pcm_float) :] = pcm_float
+        self._pcm_buffer.ins(pcm_float)
 
         if self._pcm_samples_processed < PCM_SEC:
             self._pcm_samples_processed += len(pcm16)
             if self._pcm_samples_processed < PCM_SEC:
                 return None
-
-        mel = self.mel.run([self.m_out], {self.m_in: self._pcm_buffer[None, :]})[
-            0
-        ].squeeze()
+        pcm_data = self._pcm_buffer.get()
+        mel = self.mel.run([self.m_out], {self.m_in: pcm_data[None, :]})[0].squeeze()
         mel = mel / 10.0 + 2.0
 
         patch = mel[-76:][None, :, :, None]
@@ -113,14 +203,14 @@ class OwwDetector:
         if emb is None:
             return False
 
-        self._emb_buffer = np.roll(self._emb_buffer, shift=-1, axis=0)
-        self._emb_buffer[-1, :] = emb
+        self._emb_buffer.ins(emb)
 
         if self._emb_frames_collected < self.window:
             self._emb_frames_collected += 1
             return False
 
-        stack = self._emb_buffer[None, :, :]
+        emb_data = self._emb_buffer.get()
+        stack = emb_data[None, :, :]
 
         self.last_score_raw = float(self.cls.run([self.c_out], {self.c_in: stack})[0])
         self.last_score_div = float(
